e23dd93e49a64ffc016a59e91861f71d71fccc89
thegentl3m4n <37470203+thegentl3m4n@users.noreply.github.com>
2019-11-20 19:54:47 +0530

    Update AddAudioToStartup.py

--- AddAudioToStartup.py
+++ AddAudioToStartup.py
@@ -4,7 +4,7 @@ import os, sys, shutil, time
 desktop_script = ('[Desktop Entry]', 'Name=StartupAudio', 'GenericName=startup audio', 'Comment=adds startup sound ', 'Exec=/root/.startupAudio/script.sh', 'Terminal=false', 'Type=Application', 'X-GNOME-Autostart-enabled=true')
 
 
-#this code chechs if the input file is in .ogg format or not
+#this code checks if the input file is in .ogg format or not
 
 if sys.argv[1].endswith('.ogg'):
     os.system('mkdir /root/.startupAudio/')                                      # it creates a hidden folder in root directory
@@ -17,7 +17,7 @@ if sys.argv[1].endswith('.ogg'):
     time.sleep(1)
 
 else:
-    sys.exit(sys.stderr.write('Unsupported file format.\n File must be in .ogg format\n'))          #this line gives the error if input file is not in .ogg format
+    sys.exit(sys.stderr.write('Unsupported file format.\n File must be in .ogg format\n use https://www.onlineconverter.com/mp3-to-ogg to convert your file into .ogg formant. \n'))             #this line gives the error if input file is not in .ogg format
 
 
 #this line creates an shell file which is suppose to be add on startup
